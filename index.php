2177942a9ecb716ca5873234eab95c9128f8f1bb
Serafim <fancha.aleb@yandex.ru>
2018-02-20 17:43:40 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,499 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<title>Awere</title>
+	<link href="https://fonts.googleapis.com/css?family=Oswald:400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="css/jquery.formstyler.css">
+	<link rel="stylesheet" href="css/jquery.formstyler.theme.css">
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />		
+			
+</head>
+<body>
+		
+<div class="main-wrapper">
+
+	<header class="header">
+		
+		<div class="logo">
+			<a href="#"><img src="img/logo.png" alt=""></a>
+		</div>
+
+		<ul class="main-menu">
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Men <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Bags</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Caps & Hats</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Gifts</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Grooming</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Hoodies & Sweatshirts</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Jackets & Coats</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Jeans</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Jewellery</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Joggers</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Jumpers & Cardigans</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Leather Jackets</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Long Sleeve T-Shirts</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Loungewear</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Polo Shirts</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Sunglasses</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Swimwear</a>
+						</li>
+					</ul>
+
+				</div>
+			</li>
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Women <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Shirts</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Shoes, Boots & Trainers</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Shorts</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Suits & Blazers</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Sunglasses</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Swimwear</a>
+						</li>
+					</ul>
+
+
+				</div>
+			</li>
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Kids <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">New Arrivals</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Dresses</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Tops</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Sweaters & Knits</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Jackets & Coats</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Rompers & Jumpsuits</a>
+						</li>
+					</ul>
+
+				</div>
+			</li>
+			<li class="main-menu__item dropdown">
+				<a href="#" class="main-menu__link">Accessories <span class="icon-caret"></span></a>
+				<div class="dropdown-content">
+					<ul class="dropdown-menu">
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Hair Accessories</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Sunglasses & Readers</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Socks & Tights</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Hats</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Scarves & Gloves</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Jackets & Coats</a>
+						</li>
+						<li class="dropdown-menu__item">
+							<a href="#" class="dropdown-menu__link">Rompers & Jumpsuits</a>
+						</li>
+					</ul>
+
+				</div>
+			</li>
+			<li class="main-menu__item">
+				<a href="#" class="main-menu__link">Sale</a>
+			</li>
+			<li class="main-menu__item">
+				<a href="#" class="main-menu__link">LookBook</a>
+			</li>
+			<li class="main-menu__item">
+				<a href="#" class="main-menu__link">Blog</a>
+			</li>
+		</ul>
+
+		<div class="search">
+			<form action="#">
+				<input type="text" class="form-control search__input" placeholder="Search" />
+				<input type="submit" value="&#xe90b;" class="search__button"/>
+			</form>
+		</div>
+
+		<div class="location">
+			<span class="icon-pin"></span>
+			<a href="#" class="location__link">Uzbekistan / $</a>
+		</div>
+		
+		<ul class="profile-menu">
+			<li class="profile-menu__item dropdown dropdown--right dropdown--white">
+				<a href="#" class="profile-menu__link">
+					<span class="basket">
+						<span class="icon-bag"></span>
+						<span class="badge">3</span>
+					</span>
+					<span class="b-price">$165</span>
+				</a>
+				<div class="dropdown-content">
+					<div class="cart">
+						<ul class="cart-list">
+							<li class="cart-list__item">
+								<div class="cart-list__img">
+									<a href="#"><img src="img/cart-list-img.png" alt=""></a>
+								</div>
+								<div class="cart-list__info">
+									<h6 class="product__title"><a href="#">New Balance Men's Street Backpack</a></h6>
+									<div class="cart-list__details">
+										<span class="price">$485</span>
+										<span class="product__details">
+											<span>S</span> • 
+											<span>Black</span> • 
+											<span>1 pcs</span>  
+										</span>
+									</div>
+								</div>
+								<div class="cart-list__delete">
+									<a href="#" class="js-remove-cart">
+										<span class="icon-close"><span class="path1"></span><span class="path2"></span></span>
+									</a>
+								</div>
+							</li>
+							<li class="cart-list__item">
+								<div class="cart-list__img">
+									<a href="#"><img src="img/cart-list-img1.png" alt=""></a>
+								</div>
+								<div class="cart-list__info">
+									<h6 class="product__title"><a href="#">Lyle & Scott Men's Short Sleeve Camo Pocket</a></h6>
+									<div class="cart-list__details">
+										<span class="price price--danger">$85</span>
+										<span class="product__details">  
+											<span>XXL</span> • 
+											<span>White</span> • 
+											<span>3 pcs</span>  
+										</span>
+									</div>
+								</div>
+								<div class="cart-list__delete">
+									<a href="#" class="js-remove-cart">
+										<span class="icon-close"><span class="path1"></span><span class="path2"></span></span>
+									</a>
+								</div>
+							</li>
+							<li class="cart-list__item">
+								<div class="cart-list__img">
+									<a href="#"><img src="img/cart-list-img2.png" alt=""></a>
+								</div>
+								<div class="cart-list__info">
+									<h6 class="product__title"><a href="#">Lyle & Scott Men's Short Sleeve Camo Pocket</a></h6>
+									<div class="cart-list__details">
+										<span class="price">$1 285</span>
+										<span class="product__details">
+											<span>M</span> • 
+											<span>Orange</span> • 
+											<span>89 pcs</span>  
+										</span>
+									</div>
+								</div>
+								<div class="cart-list__delete">
+									<a href="#" class="js-remove-cart">
+										<span class="icon-close"><span class="path1"></span><span class="path2"></span></span>
+									</a>
+								</div>
+							</li>
+						</ul>
+						<div class="cart-footer">
+							<span class="total">Total</span>
+							<span class="price">$3 285</span>
+							<a href="#" class="btn">Check Out</a>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="profile-menu__item dropdown dropdown--right dropdown--white">
+				<a href="#" class="profile-menu__link"><span class="icon-user"></span></a>
+				<div class="dropdown-content">
+					<ul class="user-menu">
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-user"></span>Profile</a>
+						</li>
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-heart"></span>Wishlist <span class="badge">2</span></a>
+						</li>
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-basket"></span>Orders</a>
+						</li>
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-gear"></span>Settings</a>
+						</li>
+						<li class="user-menu__item">
+							<a href="#" class="user-menu__link"><span class="icon-power"></span>Log out</a>
+						</li>
+					</ul>
+					
+				</div>
+			</li>
+		</ul>
+	</header>
+
+	<main class="main">
+		<section class="header-inner" style="background-image: url('img/header-inner.png');">
+			
+			<ul class="bread-crambs">
+				<li class="bread-crambs__item">
+					<a href="#" class="bread-crambs__link">Home</a> /
+				</li>
+				<li class="bread-crambs__item">
+					<a href="#" class="bread-crambs__link">Men</a> /
+				</li>
+			</ul>
+
+			<span class="category">men</span>
+			<h1 class="header-inner__title">Polo Shirts</h1>
+		</section>
+		<div class="tab-wrap">
+			<div class="filter">
+				<div class="filter-sidebar">
+					<button class="toggle-filter">
+						<span class="icon-sliders"></span>
+						<span class="sr-only">Filters</span>
+					</button>
+					<span class="filter__title">34 items found</span>
+				</div>
+				<div class="filter-content">
+					<span class="filter__title">Sort by</span>
+					<div class="sort">
+						<input type="radio" id="sort1" name="sort">
+						<label for="sort1" class="sort__item">Price <span class="icon-arrow-up-down"><span class="path1"></span></span></label>
+						<input type="radio" id="sort2" name="sort" checked="">
+						<label for="sort2" class="sort__item">Name <span class="icon-arrow-up-down"><span class="path1"></span></span></label>
+						<input type="radio" id="sort3" name="sort">
+						<label for="sort3" class="sort__item">Date <span class="icon-arrow-up-down"><span class="path1"></span></span></label>
+					</div>
+
+					<ul class="filter-list tabs">
+						<li class="filter-list__item">
+							<a href="#tab_1" class="filter-list__link"><span class="icon-list-2"></span></a>
+						</li>
+						<li class="filter-list__item active">
+							<a href="#tab_2" class="filter-list__link"><span class="icon-list"></span></a>
+						</li>
+					</ul>
+
+				</div>
+			</div>
+			
+			<div class="box-main">
+				<aside class="sidebar">
+					<div class="accordion">
+						<div class="accordion__head open">
+							<h5 class="accordion__title">Category</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body">
+							<ul class="check-list">
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Bags</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Belts</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Hats, Scarves & Gloves</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Sunglasses</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Watches</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Wallets</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Boat Shoes</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Boots</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Deserts & Chukkas</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Espadrilles & Sandals</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Flip Flops</label>
+								</li>
+							</ul>
+
+						</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Brand</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">
+							<ul class="check-list">
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Bags</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">Belts</label>
+								</li>
+
+							</ul>
+						</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Size</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">
+							<ul class="check-list check-list--column">
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">S</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">XL</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">M</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">XXL</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">L</label>
+								</li>
+								<li class="check-list__item">
+									<input type="checkbox" class="styler" id="check-list1">
+									<label for="check-list1" class="check-list__text">XXXL</label>
+								</li>
+
+							</ul>
+						</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Price</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">
+
+							<div id="slider-range"></div>
+							<input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+							<input type="text" id="amount1" readonly style="border:0; color:#f6931f; font-weight:bold;">
+
+						</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Brand</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">22</div>
+						<div class="accordion__head">
+							<h5 class="accordion__title">Brand</h5>
+							<span class="icon-minus"></span>
+						</div>
+						<div class="accordion__body" style="display: none;">22</div>
+					</div>
+				</aside>
+				<div class="content">
+					<div class="box-tab-cont">
+						<div class="tab-cont hide" id="tab_1">.11111..</div>
+						<div class="tab-cont " id="tab_2">.22222..</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</main>		
+	
+	<footer>
+		
+	</footer>
+	
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="js/jquery.formstyler.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
